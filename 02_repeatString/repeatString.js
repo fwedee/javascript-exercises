4857bd7112a6833295d238db378b56d0422c9e01
fw <171364056+fwedee@users.noreply.github.com>
2024-09-13 18:30:08 +0200

    Solution for 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let result = "";
+    for (let i = 0; i < num; i++){
+        result = result.concat(string)
+    }
+    return result;
 
 };
 
