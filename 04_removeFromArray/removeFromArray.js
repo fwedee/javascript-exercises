4d08216a0f29c25179a67df99edaa1a1d1fdbf4d
fw <171364056+fwedee@users.noreply.github.com>
2024-09-14 18:25:09 +0200

    Solution for exercise 04_removeFromArrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  for (let arg of args) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == arg && typeof array[i] == typeof arg) {
+        array.splice(i, 1);
+        i -= 1;
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
