f44e3aafc06a7ee42b6daec8be67339940d3bb6d
fw <171364056+fwedee@users.noreply.github.com>
2024-09-14 19:23:09 +0200

    Solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+// other option to test if int: Number.isInteger 
+const sumAll = function (min, max) {
+  if (typeof min != "number" || typeof max != "number" || min < 0 || max < 0 || min % 1 !== 0 || max % 1 !== 0) {
+    return "ERROR";
+  }
+  let sum = 0;
 
+  if (min > max) {
+    let c = min;
+    min = max;
+    max = c;
+  }
+
+  for (let i = min; i < max + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
