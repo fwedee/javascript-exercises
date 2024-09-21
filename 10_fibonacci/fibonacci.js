dd6cb3f08240536ece5ca37fb389bb5f85f7da45
fw <171364056+fwedee@users.noreply.github.com>
2024-09-21 11:50:25 +0200

    Solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (x) {
+  if (x == 1 || x == 2) {
+    return 1;
+  } else if (x == 0) {
+    return 0;
+  } else if (x < 0) {
+    return "OOPS";
+  }
+  return fibonacci(x - 1) + fibonacci(x - 2);
 };
 
 // Do not edit below this line
